a5e83ca23d73d375389cf154852ce7d9bcaad486
Nick Garcia <nicholasgarcia2003@protonmail.com>
2026-03-14 03:14:44 -0600

    added isSafe predicate
    Since the goal is to determine how many reports in the input are safe, we should start by defining what "safe" is. The predicate this commit adds determines if a report is safe based on the definition in the problem description:
    
    - The levels are either all increasing or all decreasing.
    - Any two adjacent levels differ by at least one and at most three.

--- index.py
+++ index.py
@@ -1,5 +1,75 @@
 import os
 
+def isSafe(report: [int]) -> bool:
+    """
+    A predicate for:
+        1. Levels (report elements) are all decreasing or all increasing
+        2. No two adjascent levels differ by less than 1 or more than 3.
+
+    EXAMPLES:
+
+    >>> isSafe([])
+    false
+
+    >>> isSafe([99])
+    false
+
+    >>> isSafe([0, 0])
+    false
+
+    >>> isSafe([0, 3])
+    true
+
+    >>> isSafe([3, 0])
+    true
+
+    >>> isSafe([0, 1, 0])
+    false
+
+    >>> isSafe([0, 4])
+    false
+
+    """
+
+    if len(report) < 2:
+        return false
+
+    i = 1
+    while (i < len(report)):
+        previous = report[i-1]
+        current = report[i]
+        nextItem = report[i+1]
+
+        difNextCurr = nextItem - current
+        difCurrPrev = current - previous
+
+        # Condition 1
+        if abs(difCurrPrev) > 3:
+            return false
+        elif abs(difCurrPrev) < 1:
+            return false
+
+        # Condition 1
+        if abs(difNextCurr) > 3:
+            return false
+        elif abs(difNextCurr) < 1:
+            return false
+
+        # We can infer these conditions because we already filtered out
+        # constant element pairs with pairs with condition 1
+        wasIncreasing = difCurrPrev > 0
+        wasDecreasing = difCurrPrev < 0
+        isIncreasing = difNextCurr > 0
+        isDecreasing = difNextCurr < 0
+
+        # Condition 2
+        if wasIncreasing and isIncreasing:
+            return true
+        elif wasDecreasing and isDecreasing:
+            return true
+
+    return false
+
 file = open('./input')
 
 for report in file:
